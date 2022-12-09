26493941cf27c253071353f52faad89c4cab8641
German <nemets80@mail.ru>
2022-12-10 01:47:21 +0300

    Нашли третью цифру заданного числа

---
+++ HW05_ThirdDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int digit = (int) Math.Floor(Math.Log10(a)) - 2;
+    int digit2 = (int) Math.Pow(10, digit);
+Console.WriteLine((a/digit2%10)%10);
+}
